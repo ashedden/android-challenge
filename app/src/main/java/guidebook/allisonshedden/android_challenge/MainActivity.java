4043b38fed653aabb4fb6ba69243f26a6c01dd22
Allison Shedden <ashedden@umail.ucsb.edu>
2016-04-29 13:46:26 -0700

    Prints first line from website, need to make a loop; started DataModel

--- app/src/main/java/guidebook/allisonshedden/android_challenge/MainActivity.java
+++ app/src/main/java/guidebook/allisonshedden/android_challenge/MainActivity.java
@@ -1,16 +1,121 @@
 package guidebook.allisonshedden.android_challenge;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import guidebook.allisonshedden.android_challenge.models.DataModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnHit = (Button)findViewById(R.id.btnGetInfo);
+        tvData = (TextView)findViewById(R.id.tvJsonItem);
+
+        btnHit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new JSONTask().execute("https://guidebook.com/service/v2/upcomingGuides/");
+            }
+        });
+    }
+
+    public class JSONTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                String finalJson = buffer.toString();
+
+                JSONObject parentObject = new JSONObject(finalJson);
+                JSONArray parentArray = parentObject.getJSONArray("data");
+
+                JSONObject finalObject = parentArray.getJSONObject(0);
+
+                    String startDate = finalObject.getString("startDate");
+                    String endDate = finalObject.getString("endDate");
+                    String name = finalObject.getString("name");
+                    String gburl = finalObject.getString("url");
+                    String venue = finalObject.getString("venue");
+                    String icon = finalObject.getString("icon");
+
+                    return startDate + ", " + endDate + ", " + name + ", " + gburl + ", " + venue + ", " + icon + "\n\n";
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            tvData.setText(result);
+        }
     }
 
     @Override
@@ -22,16 +127,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if(id == R.id.action_get_info){
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/guidebook/allisonshedden/android_challenge/models/DataModel.java
@@ -0,0 +1,67 @@
+package guidebook.allisonshedden.android_challenge.models;
+
+import java.util.List;
+
+/**
+ * Created by Allison on 4/29/2016.
+ */
+public class DataModel {
+
+    //Definitions
+    private String startDate;
+    private String endDate;
+    private String name;
+    private String url;
+    private String venue;
+    private String icon;
+
+    //Getters & Setters
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+}
